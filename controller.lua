a1895728113e2bff177ca6f8b69dcb5ba98c86c2
05fdtest-pixel <05fdtest@gmail.com>
2026-08-27 16:48:13 +0300

    Update controller.lua

--- controller.lua
+++ controller.lua
@@ -1,4 +1,4 @@
-local SERVER_ID = 11 -- Замени 0 на ID твоего главного компьютера-сервера!
+local SERVER_ID = 11 -- ЗАМЕНИ 0 НА ID СВОЕГО СЕРВЕРА!
 
 local modemSide = nil
 for _, side in ipairs(peripheral.getNames()) do
@@ -9,12 +9,11 @@ for _, side in ipairs(peripheral.getNames()) do
 end
 
 if not modemSide then
-    error("Wireless modem not found!")
+    error("Wireless modem not found on pocket computer!")
 end
 
 rednet.open(modemSide)
 
--- Очищаем экран в черный цвет, оставляя его пустым
 term.clear()
 term.setCursorPos(1, 1)
 
@@ -22,10 +21,9 @@ while true do
     local event, p1 = os.pullEvent()
     
     if event == "key" then
-        -- Отправляем код нажатой клавиши на сервер без вывода текста
         rednet.send(SERVER_ID, {type = "key", key = p1})
         
-        -- Экстренный выход по клавише X
+        -- Выход по клавише X
         if p1 == keys.x then
             term.clear()
             term.setCursorPos(1, 1)
