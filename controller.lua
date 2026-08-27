51fb95611f344e4561792f157f713fb2c5c16674
05fdtest-pixel <05fdtest@gmail.com>
2026-08-27 16:11:01 +0300

    Update controller.lua

--- controller.lua
+++ controller.lua
@@ -1,4 +1,4 @@
-local SERVER_ID = 0 -- Замените 0 на ID вашего сервера!
+local SERVER_ID = 11 -- Zamenis 1 na ID tvoego servera!
 
 local modemSide = nil
 for _, side in ipairs(peripheral.getNames()) do
@@ -8,17 +8,17 @@ for _, side in ipairs(peripheral.getNames()) do
     end
 end
 
-if not modemSide then error("Нет модема!") end
+if not modemSide then error("Net modem!") end
 rednet.open(modemSide)
 
 term.clear()
 term.setCursorPos(1, 1)
-print("Пульт запущен. Нажимайте W, A, S, D")
+print("Pult zapushen. Nazhimi W")
 
 while true do
     local event, p1 = os.pullEvent()
     if event == "key" then
-        print("Нажата клавиша код: " .. tostring(p1)) -- Пишет на экране пульта
+        print("Klavisha: " .. tostring(p1))
         rednet.send(SERVER_ID, {type = "key", key = p1})
     end
 end
