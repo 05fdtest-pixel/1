37d9fc990c48b43b2d40cd1e89cabcb3498ee15b
05fdtest-pixel <05fdtest@gmail.com>
2026-08-27 16:17:40 +0300

    Update controller.lua

--- controller.lua
+++ controller.lua
@@ -1,33 +1,24 @@
-local SERVER_ID = 11 -- Замени 0 на ID твоего сервера!
+local SERVER_ID = 11 -- Вставь сюда ID сервера!
 
 local modemSide = nil
 for _, side in ipairs(peripheral.getNames()) do
-    if peripheral.getType(side) == "modem" then
+    if peripheral.getType(side) == "modem" or peripheral.getType(side) == "wireless_modem" then
         modemSide = side
         break
     end
 end
 
-if not modemSide then error("No modem found!") end
+if not modemSide then error("No modem!") end
 rednet.open(modemSide)
 
 term.clear()
 term.setCursorPos(1, 1)
-print("=== CONTROLLER ===")
-print("Use W, A, S, D to move")
-print("Press X to exit")
+print("Testing keys...")
 
 while true do
     local event, p1 = os.pullEvent()
-    
     if event == "key" then
+        print("Sent key: " .. tostring(p1))
         rednet.send(SERVER_ID, {type = "key", key = p1})
-        
-        if p1 == keys.x then
-            term.clear()
-            term.setCursorPos(1, 1)
-            print("Controller stopped.")
-            break
-        end
     end
 end
