9c5bc5b88860ee41dddc7c4db773fcfa4f7f0081
05fdtest-pixel <05fdtest@gmail.com>
2026-08-27 17:02:44 +0300

    Update controller.lua

--- controller.lua
+++ controller.lua
@@ -1,33 +1,40 @@
-local SERVER_ID = 11 -- ЗАМЕНИ 0 НА ID СВОЕГО СЕРВЕРА!
-
-local modemSide = nil
-for _, side in ipairs(peripheral.getNames()) do
-    if peripheral.getType(side) == "modem" or peripheral.getType(side) == "wireless_modem" then
-        modemSide = side
-        break
-    end
-end
-
-if not modemSide then
-    error("Wireless modem not found on pocket computer!")
-end
-
-rednet.open(modemSide)
+local modem = peripheral.find("modem")
+local CHANNEL = 15
 
 term.clear()
-term.setCursorPos(1, 1)
+
+local keyMap = {
+    [keys.w] = "forward",
+    [keys.s] = "back",
+    [keys.a] = "left",
+    [keys.d] = "right"
+}
+
+local activeTouch = nil
 
 while true do
-    local event, p1 = os.pullEvent()
+    local event, p1, p2, p3 = os.pullEvent()
     
-    if event == "key" then
-        rednet.send(SERVER_ID, {type = "key", key = p1})
-        
-        -- Выход по клавише X
-        if p1 == keys.x then
-            term.clear()
-            term.setCursorPos(1, 1)
-            break
+    if event == "key" and keyMap[p1] then
+        modem.transmit(CHANNEL, CHANNEL, { action = keyMap[p1], state = true })
+    elseif event == "key_up" and keyMap[p1] then
+        modem.transmit(CHANNEL, CHANNEL, { action = keyMap[p1], state = false })
+    elseif event == "mouse_click" then
+        local x, y = p2, p3
+        local action = nil
+        if y >= 1 and y <= 5 then
+            if x >= 1 and x <= 5 then action = "forward"
+            elseif x >= 6 and x <= 10 then action = "left"
+            elseif x >= 11 and x <= 15 then action = "back"
+            elseif x >= 16 and x <= 20 then action = "right"
+            end
         end
+        if action then
+            activeTouch = action
+            modem.transmit(CHANNEL, CHANNEL, { action = action, state = true })
+        end
+    elseif event == "mouse_up" and activeTouch then
+        modem.transmit(CHANNEL, CHANNEL, { action = activeTouch, state = false })
+        activeTouch = nil
     end
 end
