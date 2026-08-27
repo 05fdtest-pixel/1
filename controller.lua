5221787a83382c08a338f66049fd7cdd4656b3c8
05fdtest-pixel <05fdtest@gmail.com>
2026-08-27 16:23:03 +0300

    Update controller.lua

--- controller.lua
+++ controller.lua
@@ -1,6 +1,6 @@
-local SERVER_ID = 11 -- Замени 0 на ID твоего главного компьютера!
+local SERVER_ID = 11 -- Замени 0 на ID твоего главного компьютера-сервера!
 
--- Автоматический поиск модема на карманном ПК
+-- Автоматический поиск беспроводного модема на карманном ПК
 local modemSide = nil
 for _, side in ipairs(peripheral.getNames()) do
     if peripheral.getType(side) == "modem" or peripheral.getType(side) == "wireless_modem" then
@@ -17,15 +17,24 @@ rednet.open(modemSide)
 
 term.clear()
 term.setCursorPos(1, 1)
-print("=== PULT ZAPUSHEN ===")
-print("Nazhimay W, A, S, D")
+print("=== REMOTE CONTROLLER ===")
+print("Use W, A, S, D to move")
+print("Press X to exit")
 
 while true do
     local event, p1 = os.pullEvent()
     
     if event == "key" then
-        -- Выводим на экран пульта для проверки
-        print("Key sent: " .. tostring(p1))
+        -- Отправляем код нажатой клавиши на сервер
         rednet.send(SERVER_ID, {type = "key", key = p1})
+        print("Key sent: " .. tostring(p1))
+        
+        -- Выход по клавише X
+        if p1 == keys.x then
+            term.clear()
+            term.setCursorPos(1, 1)
+            print("Controller stopped.")
+            break
+        end
     end
 end
