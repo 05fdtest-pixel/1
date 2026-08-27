1686c809e1c99d91e9299faa699a8120540096e4
05fdtest-pixel <05fdtest@gmail.com>
2026-08-27 16:02:04 +0300

    Create controller.lua

---
+++ controller.lua
@@ -0,0 +1,24 @@
+-- Карманный ПК (Беспроводной пульт)
+local modem = peripheral.find("modem")
+if not modem then error("Нужен беспроводной модем!") end
+rednet.open(peripheral.getName(modem))
+
+local SERVER_ID = 0 -- Замените 0 на ID главного компьютера!
+
+term.setBackgroundColor(colors.black)
+term.clear()
+
+while true do
+    local event, p1 = os.pullEvent()
+    
+    if event == "key" then
+        rednet.send(SERVER_ID, {type = "key", key = p1})
+        
+        if p1 == keys.x then
+            term.clear()
+            term.setCursorPos(1, 1)
+            print("Пульт выключен.")
+            break
+        end
+    end
+end
